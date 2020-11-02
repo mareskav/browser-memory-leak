a9799ef05db71ff8812bef385bc47790e4b3af3e
mareskav <mareska.vaclav@centrum.cz>
2020-11-03 00:23:44 +0100

    Add initial changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Navbar, Nav, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [stringList, setStringList] = useState([]);
+
+  const onLeakStart = () => {
+    const x = Array(1000000).join("x");
+    for (let i = 0; i < 10000000; i++) {
+      setStringList([...stringList, x]);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand href="#home">Browser Memory Leak App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <div className="app-container">
+        <Button variant="primary" onClick={onLeakStart}>
+          Render many "x"
+        </Button>
+        <div className="loader" />
+        <br />
+        <div id="test">
+          {stringList.map((longString, index) => {
+            return <p key={index}>{longString}</p>;
+          })}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
